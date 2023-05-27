59f910be46f346e45b34b547c47f654ebc4f3a10
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2023-05-27 12:44:05 +0800

    Refactored the code.

--- ShowCompanionRequirementsManager.cs
+++ ShowCompanionRequirementsManager.cs
@@ -6,15 +6,12 @@
 
         public static ShowCompanionRequirementsManager Current => _showCompanionRequirementsManager;
 
-        public int RequiredTroopCount { get; set; }
-
         public int MinimumTier { get; set; }
 
         public bool MountedRequired { get; set; }
 
-        public void SetRequiredTroops(int requiredTroopCount, int minimumTier, bool mountedRequired)
+        public void SetTierAndMounted(int minimumTier, bool mountedRequired)
         {
-            RequiredTroopCount = requiredTroopCount;
             MinimumTier = minimumTier;
             MountedRequired = mountedRequired;
         }
