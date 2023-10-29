51178c48bd69d453885469b0b0e6d6bae8d25db7
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2023-10-29 12:43:29 +0800

    Add files via upload

---
+++ UsefulSkipsCharacterCreationState.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem.CharacterCreationContent;
+using TaleWorlds.Core;
+
+namespace UsefulSkips
+{
+    [HarmonyPatch(typeof(CharacterCreationState), "NextStage")]
+    public class UsefulSkipsCharacterCreationState
+    {
+        public static void Prefix(CharacterCreationState __instance)
+        {
+            if (UsefulSkipsSettings.Instance.ShouldSkipCharacterCreation)
+            {
+                // Set a random culture.
+                __instance.CurrentCharacterCreationContent.SetSelectedCulture(__instance.CurrentCharacterCreationContent.GetCultures().GetRandomElementInefficiently(), __instance.CharacterCreation);
+            }
+        }
+
+        public static void Postfix(CharacterCreationState __instance)
+        {
+            if (UsefulSkipsSettings.Instance.ShouldSkipCharacterCreation)
+            {
+                // Skip character creation.
+                __instance.FinalizeCharacterCreation();
+            }
+        }
+    }
+}
