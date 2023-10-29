65509b8e0ec75f60aac154999d38314188ef4f74
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2023-10-29 12:24:30 +0800

    Refactored the code.

--- ShowCompanionRequirementsCollection.cs
+++ ShowCompanionRequirementsCollection.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,10 +15,15 @@ using TaleWorlds.Localization;
 
 namespace ShowCompanionRequirements
 {
-    [HarmonyPatch(typeof(TooltipRefresherCollection), "RefreshHeroTooltip")]
+    [HarmonyPatch]
     public static class ShowCompanionRequirementsCollection
     {
-        public static void Postfix(string ___ExtendKeyId, PropertyBasedTooltipVM propertyBasedTooltipVM, object[] args)
+        private static int _minimumTier;
+        private static bool _mountedRequired;
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(TooltipRefresherCollection), "RefreshHeroTooltip")]
+        public static void Postfix1(string ___ExtendKeyId, PropertyBasedTooltipVM propertyBasedTooltipVM, object[] args)
         {
             IssueBase issue = ((Hero)args[0]).Issue;
 
@@ -122,8 +128,8 @@ namespace ShowCompanionRequirements
                     propertyBasedTooltipVM.AddProperty(new TextObject("{=ShowCompanionRequirements07}Troops Required").ToString(), " ", 0, TooltipProperty.TooltipPropertyFlags.None);
                     propertyBasedTooltipVM.AddProperty("", "", 0, TooltipProperty.TooltipPropertyFlags.RundownSeperator);
                     propertyBasedTooltipVM.AddProperty(new TextObject("{=ShowCompanionRequirements08}Number").ToString(), lowestTroopCount.ToString(), 0, TooltipProperty.TooltipPropertyFlags.None);
-                    propertyBasedTooltipVM.AddProperty(new TextObject("{=ShowCompanionRequirements09}Minimum Tier").ToString(), isSpecial ? ShowCompanionRequirementsManager.Current.MinimumTier.ToString() : "2", 0, TooltipProperty.TooltipPropertyFlags.None);
-                    propertyBasedTooltipVM.AddProperty(new TextObject("{=ShowCompanionRequirements10}Cavalry").ToString(), !isSpecial && ShowCompanionRequirementsManager.Current.MountedRequired ? new TextObject("{=ShowCompanionRequirements11}Yes").ToString() : new TextObject("{=ShowCompanionRequirements12}No").ToString(), 0, TooltipProperty.TooltipPropertyFlags.None);
+                    propertyBasedTooltipVM.AddProperty(new TextObject("{=ShowCompanionRequirements09}Minimum Tier").ToString(), !isSpecial ? _minimumTier.ToString() : "2", 0, TooltipProperty.TooltipPropertyFlags.None);
+                    propertyBasedTooltipVM.AddProperty(new TextObject("{=ShowCompanionRequirements10}Cavalry").ToString(), !isSpecial && _mountedRequired ? new TextObject("{=ShowCompanionRequirements11}Yes").ToString() : new TextObject("{=ShowCompanionRequirements12}No").ToString(), 0, TooltipProperty.TooltipPropertyFlags.None);
                     propertyBasedTooltipVM.AddProperty(string.Empty, string.Empty, -1, TooltipProperty.TooltipPropertyFlags.None);
                     // Add the issue's required skills to the issue giver's tooltip.
                     propertyBasedTooltipVM.AddProperty(new TextObject("{=ShowCompanionRequirements13}Skills Required").ToString(), new TextObject("{=ShowCompanionRequirements14}(One of these)").ToString(), 0, TooltipProperty.TooltipPropertyFlags.None);
@@ -146,6 +152,14 @@ namespace ShowCompanionRequirements
                     propertyBasedTooltipVM.AddProperty(new TextObject("{=ShowCompanionRequirements17}Chance of Success").ToString(), highestSuccessRate.ToString() + "%", 0, TooltipProperty.TooltipPropertyFlags.None);
                 }
             }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(QuestHelper), "CheckRosterForAlternativeSolution")]
+        public static void Postfix2(int minimumTier, bool mountedRequired)
+        {
+            _minimumTier = minimumTier;
+            _mountedRequired = mountedRequired;
         }
     }
 }
